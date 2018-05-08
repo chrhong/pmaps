9e47efe675ad3d1289100acfa3c82f3a962660e8
chrhong <christian.hong@nokia-sbell.com>
2018-05-08 14:51:03 +0800

    Add file name support; Add demo; use range() instead while.

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # pmaps
-This is tool to show `/proc/pid/maps` as picture, easy to find the free virtual address area. 
+This is tool to show `/proc/pid/maps` as picture, easy to find the free virtual address area.
 Especially used to debug the continues virtual address not enough issue.
 
-Usage:
-./pmaps.py (-h) (-f) (-b [size]) (-k [keyword]) (-long [bitsSize])
+## Usage:
+./pmaps.py (-h) (-f [file]) (-a) (-b [size]) (-k [keyword]) (-long [bitsSize])
 
 * -h : show help
-* -f : show free blocks
+* -f : the file need to be checked, default: use ./example.maps
+* -a : show avaliable free blocks
 * -b : show free blocks size bigger than given
 * -k : show blocks owner have the give keyword
 * -l : set long bit size(64/32), used when maps file is not match with current env
 * If no parameters given, then show the whole virtual address maps as a pic
+
+## Demo:
+![](demo.png)
--- pmaps.py
+++ pmaps.py
@@ -6,18 +6,19 @@ import commands
 import math
 
 LONG_BIT = int(commands.getoutput("getconf LONG_BIT"))
+TARGET_FILE = 'example.maps'
 
-RED_BLOCK =   "\033[1;31;40m|-------------------------------|\033[0m"
-EMPTY_BLOCK = "\033[1;37;40m|\t|\033[0m"
-GREEN_BLOCK = "\033[1;32;40m|+++++++++++++++++++++++++++++++|\033[0m"
+BUSY_BLOCK = "\033[1;31;40m|-------------------------------|\033[0m" # red block
+FREE_BLOCK = "\033[1;32;40m|+++++++++++++++++++++++++++++++|\033[0m" # green block
 
 USAGE_EXAMPLE = \
     "This is tool to show the virtual address maps as a visiable pic\n" \
     "Usage:\n"\
-    "./pmaps.py (-h) (-f) (-b [size]) (-k [keyword]) (-long [bitsSize])\n" \
+    "./pmaps.py (-h) (-f [file]) (-a) (-b [size]) (-k [keyword]) (-l [bitsSize])\n" \
     "\n" \
     "-h : show help\n" \
-    "-f : show free blocks\n" \
+    "-f : the file need to be checked, default: use ./example.maps\n" \
+    "-a : show avaliable free blocks\n" \
     "-b : show free blocks size bigger than given\n" \
     "-k : show blocks owner have the give keyword\n" \
     "-l : set long bit size(64/32), used when maps file is not match with current env\n" \
@@ -30,7 +31,7 @@ def print_green(log_str):
     print("\033[1;32;40m\t%s\033[0m" % log_str)
 
 def get_maps_line(num):
-    return commands.getoutput("cat virtualaddr.txt | awk '{print $%d}'" % num).split('\n')
+    return commands.getoutput("cat %s | awk '{print $%d}'" % (TARGET_FILE, num)).split('\n')
 
 def get_virtaddr_list():
     vaddr_start = []
@@ -65,11 +66,11 @@ def get_size_list(vm_start, vm_end):
     i = 0
     size = 0
     sz_list = []
-    while i < len(vm_start):
+
+    for i in range(len(vm_start)):
         size = long(vm_end[i], 16) - long(vm_start[i], 16)
         #print(hex(size))
         sz_list.append(hex(int(size)))
-        i += 1
 
     return sz_list
 
@@ -79,34 +80,31 @@ def get_free_list(max_addr, vm_start, vm_end):
     size = 0
     sz_list = []
     #0~first addr
-    size = long(vm_start[i+1], 16) - 0
+    size = long(vm_start[i], 16) - 0
     sz_list.append(hex(size))
 
-    while i < len(vm_start) - 1:
+    for i in range(len(vm_start)-1):
         size = long(vm_start[i+1], 16) - long(vm_end[i], 16)
         sz_list.append(hex(size))
-        i += 1
 
     #last addr ~ end
-    size = max_addr - long(vm_end[i], 16)
+    size = max_addr - long(vm_end[-1], 16)
     sz_list.append(hex(size))
 
     return sz_list
 
 def draw_pic(vm_start, vm_end, sz_list, free_list, max_addr, owner_list):
-    print(GREEN_BLOCK + ' 0x0 \t None')
+    print(FREE_BLOCK + ' 0x0 \t None')
     print_green(free_list[0])
 
-    i = 0
-    while i < len(vm_start):
-        print(RED_BLOCK + ' 0x' + vm_start[i] + '\t' + owner_list[i])
+    for i in range(len(vm_start)):
+        print(BUSY_BLOCK + ' 0x' + vm_start[i] + '\t' + owner_list[i])
         print_red(sz_list[i])
         if free_list[i+1] != '0x0L':
-            print(GREEN_BLOCK + ' 0x' + vm_end[i] + '\t None')
+            print(FREE_BLOCK + ' 0x' + vm_end[i] + '\t None')
             print_green(free_list[i+1])
-        i += 1
 
-    print(RED_BLOCK + ' ' + hex(max_addr))
+    print(BUSY_BLOCK + ' ' + hex(max_addr))
     return
 
 def show_help(vm_start, vm_end, sz_list, free_list, max_addr, owner_list, params):
@@ -115,83 +113,86 @@ def show_help(vm_start, vm_end, sz_list, free_list, max_addr, owner_list, params
 
 def show_avaliable_block(vm_start, vm_end, sz_list, free_list, max_addr, owner_list, params):
     if free_list[0] != '0x0L':
-        print(GREEN_BLOCK + ' 0x0')
+        print(FREE_BLOCK + ' 0x0')
         print_green(free_list[0])
-        print(GREEN_BLOCK + ' 0x' + vm_start[1])
+        print(FREE_BLOCK + ' 0x' + vm_start[0])
         print('')
 
-    i = 1
-    while i < len(free_list) - 1:
+    #notice i = len(free_list)-2 at for end
+    for i in range(1, len(free_list) - 1):
         if free_list[i] != '0x0L':
-            print(GREEN_BLOCK + ' 0x' + vm_end[i-1])
+            print(FREE_BLOCK + ' 0x' + vm_end[i-1])
             print_green(free_list[i])
-            print(GREEN_BLOCK + ' 0x' + vm_start[i])
+            print(FREE_BLOCK + ' 0x' + vm_start[i])
             print('')
-        i += 1
 
     if free_list[-1] != '0x0L':
-        print(GREEN_BLOCK + ' 0x' + vm_end[i-1])
+        print(FREE_BLOCK + ' 0x' + vm_end[-1])
         print_green(free_list[-1])
-        print(GREEN_BLOCK + ' ' + hex(max_addr))
+        print(FREE_BLOCK + ' ' + hex(max_addr))
         print('')
 
 def show_blocks_bigger_than_size(vm_start, vm_end, sz_list, free_list, max_addr, owner_list, params):
     given_size = int(params[1], 16) if '0x' in params[1] else int(params[1])
 
     if free_list[0] != '0x0L' and long(free_list[0], 16) > given_size:
-        print(GREEN_BLOCK + ' 0x0')
+        print(FREE_BLOCK + ' 0x0')
         print_green(free_list[0])
-        print(GREEN_BLOCK + ' 0x' + vm_start[1])
+        print(FREE_BLOCK + ' 0x' + vm_start[1])
         print('')
 
-    i = 1
-    while i < len(free_list) - 1:
+    #notice i = len(free_list)-2 at for end
+    for i in range(1, len(free_list) - 1):
         if free_list[i] != '0x0L' and long(free_list[i], 16) > given_size:
-            print(GREEN_BLOCK + ' 0x' + vm_end[i-1])
+            print(FREE_BLOCK + ' 0x' + vm_end[i-1])
             print_green(free_list[i])
-            print(GREEN_BLOCK + ' 0x' + vm_start[i])
+            print(FREE_BLOCK + ' 0x' + vm_start[i])
             print('')
-        i += 1
 
     if free_list[-1] != '0x0L' and long(free_list[-1], 16) > given_size:
-        print(GREEN_BLOCK + ' 0x' + vm_end[i-1])
+        print(FREE_BLOCK + ' 0x' + vm_end[-1])
         print_green(free_list[-1])
-        print(GREEN_BLOCK + ' ' + hex(max_addr))
+        print(FREE_BLOCK + ' ' + hex(max_addr))
         print('')
 
 def filter_owner_with_key(vm_start, vm_end, sz_list, free_list, max_addr, owner_list, params):
-    i = 1
-    while i < len(owner_list):
+    for i in range(1, len(owner_list)):
         if params[1] in owner_list[i]:
-            print(RED_BLOCK + ' 0x' + vm_start[i] + '\t' + owner_list[i])
+            print(BUSY_BLOCK + ' 0x' + vm_start[i] + '\t' + owner_list[i])
             print_red(sz_list[i])
-            print(RED_BLOCK + ' 0x' + vm_end[i])
+            print(BUSY_BLOCK + ' 0x' + vm_end[i])
             print('')
-        i += 1
 
     return
 
+def parse_args_by_key(params_list, key):
+    key_value = ''
+    if key in params_list:
+        i = params_list.index(key)
+        key_value = params_list[i + 1]
+        del params_list[i + 1]
+        del params_list[i]
+        #print(params_list)
+    return key_value
+
 ACTION_MAPS = {
     '-h' : show_help,
-    '-f' : show_avaliable_block,
+    '-a' : show_avaliable_block,
     '-b' : show_blocks_bigger_than_size,
     '-k' : filter_owner_with_key
 }
 
 def _main():
     params_list = sys.argv[1:]
-    if '-l' in params_list:
-        i = params_list.index('-l')
-        long_bit = float(params_list[i + 1])
-        del params_list[i + 1]
-        del params_list[i]
-        #print(params_list)
-    else:
-        long_bit = LONG_BIT
 
-    #print(LONG_BIT)
+    global LONG_BIT
+    global TARGET_FILE
+    long_bit_str = parse_args_by_key(params_list, '-l')
+    target_file  = parse_args_by_key(params_list, '-f')
+    LONG_BIT = float(long_bit_str) if long_bit_str else LONG_BIT
+    TARGET_FILE = target_file if target_file else TARGET_FILE
 
-    max_addr = long(math.pow(2, long_bit)) - 1
+    max_addr = long(math.pow(2, LONG_BIT)) - 1
     vm_start,vm_end = get_virtaddr_list()
     sz_list = get_size_list(vm_start, vm_end)
     free_list = get_free_list(max_addr, vm_start, vm_end)
